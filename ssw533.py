3cdc5f3293820e61b25f71ff77d1c6c382470043
cy <cylee820621@gmail.com>
2019-10-28 22:38:22 -0400

    add text_sentiment function

--- ssw533.py
+++ ssw533.py
@@ -1,12 +1,29 @@
 import nltk
 import pandas as pd
-from pandas import ExcelWriter
 from pandas import ExcelFile
 import matplotlib.pyplot as plt
+from textblob import TextBlob
+
+def Contributor_total_sentiment(ex):
+    contributor_sentiment_dict = dict()
+    for i in ex.index:
+        text = TextBlob(ex['comment_content'][i])
+        if ex['commenter'][i] not in contributor_sentiment_dict:
+            contributor_sentiment_dict[ex['commenter'][i]] = [text.sentiment.polarity]
+        else:
+            contributor_sentiment_dict[ex['commenter'][i]].append(text.sentiment.polarity)
+    return contributor_sentiment_dict
+
+
+def sentiment(text):
+    process_text = TextBlob(text)
+    return process_text.sentiment
 
 def get_commenter_comments_dict(ex):
     """
     df用法= df[第幾欄][第幾列]
+    輸入excel
+    輸出dict{'contributor1': #comments } }
     """
     dict_contributor_comments = dict()
     for i in ex.index:
@@ -27,14 +44,13 @@ def output_excel(df):
     for key, value in df.items():
         a.append(key)
         b.append(value)
-
     dit = {'Contributor':a, 'Numbers of comments':b}
-    writer = pd.ExcelWriter('output.xlsx',engine='xlsxwriter')
+    writer = pd.ExcelWriter('output_Contributor_comments.xlsx',engine='xlsxwriter')
     df = pd.DataFrame(dit)
     #columns参数用于指定生成的excel中列的顺序
     df.to_excel(writer, columns=['Contributor','Numbers of comments'], index=False,encoding='utf-8',sheet_name='Sheet')
     writer.save()
-    df.to_csv(r'./1.csv',columns=['Contributor','Numbers of comments'],index=False,sep=',')
+    df.to_csv(r'output_Contributor_comments.csv',columns=['Contributor','Numbers of comments'],index=False,sep=',')
 
 
 if __name__ == "__main__":
@@ -42,3 +58,5 @@ if __name__ == "__main__":
     commenter_comments_dict = get_commenter_comments_dict(excel)
     print(commenter_comments_dict)
     output_excel(commenter_comments_dict)
+    sentiment_dict = Contributor_total_sentiment(excel)
+    print(sentiment_dict)
